0b3431d1700ae75ae6796029a51322cb4b18b08d
Vilai Vang <vvangx392@gmail.com>
2020-02-19 02:11:13 -0600

    update3

--- src/App.js
+++ src/App.js
@@ -1,66 +1,55 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import {
-  Container,
-  Navbar,
-  NavbarBrand,
-  NavDropdown,
-  Nav,
-  Form,
-  FormControl,
-  Button
+    Container,
+    Navbar,
+    NavbarBrand,
+    NavDropdown,
+    Nav,
+    Form,
+    FormControl,
+    Button
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Cards from "./component/Cards";
 import Timer from "react-compound-timer";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="background">
-      <div className="container-fluid">
-        <div className="row">
-          <Navbar
-            bg="light"
-            expand="lg"
-            className="navbar w-100 navbar-dark bg-dark"
-          >
-            <Navbar.Brand>AFFINITY PLUS</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto"></Nav>
+    return (
+        <div className="background">
+            <div className="container-fluid">
+                <div className="row">
+                    <Navbar bg="light" expand="lg" className="navbar w-100 navbar-dark bg-dark">
+                        <Navbar.Brand>AFFINITY PLUS</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto"></Nav>
 
-              <Form inline>
-                <FormControl
-                  type="text"
-                  placeholder="Search"
-                  className="mr-sm-2"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
-            </Navbar.Collapse>
-          </Navbar>
+                            <Form inline>
+                                <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Navbar>
+                    
+<div class="sidebar">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+
+
+<div class="content">
+
+</div>
+                </div>
+                <br></br>
+                <Cards/>
+            </div>
         </div>
-        <div className="row timercontainer">
-          <div className="timer">
-            <Timer initialTime={55000} direction="backward">
-              {() => (
-                <React.Fragment>
-                  <Timer.Days /> days
-                  <Timer.Hours /> hours
-                  <Timer.Minutes /> minutes
-                  <Timer.Seconds /> seconds
-                  <Timer.Milliseconds /> milliseconds
-                </React.Fragment>
-              )}
-            </Timer>
-          </div>
-        </div>
-        <Cards />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,116 +1,120 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./Cards.css";
 
-import { Card, CardImg, CardText, CardTitle, CardSubtitle } from "reactstrap";
-import { CardBody, Button } from "reactstrap";
+import {
+    Card,
+    CardImg,
+    CardText,
+    CardTitle,
+    CardSubtitle
+} from "reactstrap";
+import {CardBody, Button} from "reactstrap";
 import Form from "react-bootstrap/Form";
 import TableTemplate from './tabletemplate';
 import Charttemplate from "./charttemplate";
 
 
-
 class Cards extends Component {
-  jiracomplete = [
-    {
-      jiraNumber: '5432',
-      jiraTitle: 'qhaskldas',
-      status: 'Complete'
-    },
-    {
-      jiraNumber: '9876',
-      jiraTitle: 'jklwlqkeqw',
-      status: 'Complete'
+    jiracomplete = [
+        {
+            jiraNumber: '5432',
+            jiraTitle: 'qhaskldas',
+            status: 'Complete'
+        }, {
+            jiraNumber: '9876',
+            jiraTitle: 'jklwlqkeqw',
+            status: 'Complete'
+        }
+    ];
+    jiradata = [
+        {
+            jiraNumber: '1234',
+            jiraTitle: 'jititle',
+            status: 'Complete'
+        }, {
+            jiraNumber: '111',
+            jiraTitle: 'jitidfdfdtle',
+            status: 'In Progress'
+        }
+    ];
+    render() {
+        return (
+            <div className="main-card">
+                <div>
+                    <div className="row">
+                        <div className="col-md-4 form-container">
+                            <Card className='mainform'>
+                                <CardBody>
+                                    <CardTitle>Card Title</CardTitle>
+                                    <CardText></CardText>
+                                    <Form >
+                                        <Form.Group controlId="exampleForm.ControlInput1">
+                                            <Form.Label>Email address</Form.Label>
+                                            <Form.Control type="email" placeholder="name@example.com"/>
+                                        </Form.Group>
+                                        <Form.Group controlId="exampleForm.ControlSelect1">
+                                            <Form.Label>Example select</Form.Label>
+                                            <Form.Control as="select">
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                            </Form.Control>
+                                        </Form.Group>
+                                        <Form.Group controlId="exampleForm.ControlSelect2">
+                                            <Form.Label>Example multiple select</Form.Label>
+                                            <Form.Control as="select" multiple>
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                            </Form.Control>
+                                        </Form.Group>
+                                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                                            <Form.Label>Example textarea</Form.Label>
+                                            <Form.Control as="textarea" rows="3"/>
+                                        </Form.Group>
+                                        <Button>Button</Button>
+                                    </Form>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-md-4 jira-map">
+                            <Card>
+                                <CardBody className='backcard'>
+                                    <CardTitle>Upcoming</CardTitle>
+
+                                    <TableTemplate value={
+                                        this.jiradata
+                                    }/>
+                                </CardBody>
+                            </Card>
+                            <br></br>
+                            <Card>
+                                <CardBody className='backcard'>
+                                    <CardTitle className='title'>Completed</CardTitle>
+
+                                    <TableTemplate value={
+                                        this.jiracomplete
+                                    }/>
+                                </CardBody>
+                            </Card>
+                            <br></br>
+                            <Card>
+                                <CardBody className='backcard'>
+                                    <CardTitle>Graph</CardTitle>
+
+                                    <Charttemplate/>
+                                </CardBody>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
-  ];
-  jiradata = [
-  {
-    jiraNumber: '1234',
-    jiraTitle: 'jititle',
-    status: 'Complete'
-  },
-  {
-    jiraNumber: '111',
-    jiraTitle: 'jitidfdfdtle',
-    status: 'In Progress'
-  }
-];
-  render() {
-    return (
-      <div className="main-card">
-        <div>
-          <div className="row">
-            <div className="col-md-4 form-container">
-              <Card>
-                <CardBody>
-                  <CardTitle>Card Title</CardTitle>
-                  <CardText></CardText>
-                  <Form>
-                    <Form.Group controlId="exampleForm.ControlInput1">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control
-                        type="email"
-                        placeholder="name@example.com"
-                      />
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlSelect1">
-                      <Form.Label>Example select</Form.Label>
-                      <Form.Control as="select">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                      </Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlSelect2">
-                      <Form.Label>Example multiple select</Form.Label>
-                      <Form.Control as="select" multiple>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                      </Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                      <Form.Label>Example textarea</Form.Label>
-                      <Form.Control as="textarea" rows="3" />
-                    </Form.Group>
-                    <Button>Button</Button>
-                  </Form>
-                </CardBody>
-              </Card>
-            </div>
-            <div className="col-md-4 jira-map">
-              <Card>
-                <CardBody>
-                  <CardTitle>Upcoming</CardTitle>
-                  
-                 <TableTemplate value={this.jiradata}/>
-                </CardBody>
-              </Card>
-
-              <Card>
-                <CardBody>
-                  <CardTitle>Completed</CardTitle>
-                  
-                  <TableTemplate value={this.jiracomplete}/>
-                </CardBody>
-              </Card>
-
-              <Card>
-                <CardBody>
-                  <CardTitle>Graph</CardTitle>
-                  
-                 <Charttemplate />
-                </CardBody>
-              </Card>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default Cards;
